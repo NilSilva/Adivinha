f886efc0fe53edd9e37efeb853b4ba60ad6710f3
Nil Silva <nil_silva_12@hotmail.com>
2019-04-08 11:34:26 +0100

    Adicionado o teste para verificar se os numeros são criados corretamente.

--- app/src/main/java/nil/s/adivinha/GeradorNumerosAdivinhar.java
+++ app/src/main/java/nil/s/adivinha/GeradorNumerosAdivinhar.java
@@ -2,9 +2,17 @@ package nil.s.adivinha;
 
 import java.util.Random;
 
+/**
+ * Esta classe gerar numeros entre 1 e 10
+ */
+
 public class GeradorNumerosAdivinhar {
     private Random random = new Random();
 
+    /**
+     * Obtem o proximo numero a adivinhar
+     * @return um numero entre 1 e 10
+     */
     public int getProximoNumeroAdivinhar(){
         return random.nextInt(10);//Erro esta a dar um numero entre 0 e 9
     }
---
+++ app/src/test/java/nil/s/adivinha/GeradorNumerosAdivinharUnitTest.java
@@ -0,0 +1,23 @@
+package nil.s.adivinha;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class GeradorNumerosAdivinharUnitTest {
+    @Test
+    public void getProximoNumeroAdivinhar_isCorrect() {
+        GeradorNumerosAdivinhar gerador = new GeradorNumerosAdivinhar();
+
+        for(int i = 0; i < 100000; i++) {
+            int num = gerador.getProximoNumeroAdivinhar();
+
+            assertTrue(num >= 1 && num <= 10);
+        }
+    }
+}
